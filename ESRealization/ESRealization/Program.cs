459011fa578c672d7133c0860a08b37e7abe0282
blessed-one <shakirovdanis05@gmail.com>
2024-05-06 11:18:30 +0300

    _

--- ESRealization/ESRealization/Program.cs
+++ ESRealization/ESRealization/Program.cs
@@ -5,13 +5,6 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        var sieveManager = new SieveManager();
-        
-        var watch = new Stopwatch();
-        watch.Start();
-        var primes = sieveManager.FindPrimes(10_000_000);
-        watch.Stop();
-
-        Console.WriteLine(watch.ElapsedMilliseconds);
+        new SieveManager().FindPrimes(10_000_000).Print();
     }
 }
