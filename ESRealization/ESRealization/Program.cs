c00bee464a1c9ca4878c9e932805dbfbec6ff5ff
blessed-one <shakirovdanis05@gmail.com>
2024-05-09 21:38:49 +0300

    я опять хз че происходит

--- ESRealization/ESRealization/Program.cs
+++ ESRealization/ESRealization/Program.cs
@@ -5,13 +5,12 @@ public static class Program
 {
     public static void Main(string[] args)
     {
-        var sieveManager = new SieveManager();
-        
-        var watch = new Stopwatch();
-        watch.Start();
-        var primes = sieveManager.FindPrimes(10_000_000);
-        watch.Stop();
-
-        Console.WriteLine(watch.ElapsedMilliseconds);
+<<<<<<< HEAD
+        var manager = new SieveManager();
+        manager.FindPrimes(100);
+        manager.GetSteps().Print();
+=======
+        new SieveManager().FindPrimes(10_000_000).Print();
+>>>>>>> 459011fa578c672d7133c0860a08b37e7abe0282
     }
 }
